86922ac90e201db9b0b93b83e3f3186240078e96
Miloš Ljubinković <milos.ljubinkovic@helbiz.com>
2023-12-28 12:56:20 +0100

    permissions and boilerplate

---
+++ blog/src/app.js
@@ -0,0 +1,11 @@
+'use strict'
+const express = require('express')
+const app = express()
+app.use('/', function (req, res, next) { res.status(200).json({ healthier: true }); })
+
+app.use(function (err, req, res, next) {
+    console.error(err);
+    res.status(500).json({ message: "Internal error", key: "error" })
+});
+
+module.exports = app;
